18aa9526143a7255b45bb59d9390b482b239dba0
Samyak Jain <jainsamyak2002.sj@gmail.com>
2023-01-16 21:11:49 +0530

    Removed Errors, added icons, changed Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,8 +9,8 @@ export default function Navbar(props) {
     <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
       <div className="container-fluid">
         <Link className="navbar-brand" to="/">
-          <img src="https://img.icons8.com/color/512/txt.png" alt="text-logo" width="50" height="47" class="d-inline-block align-text-center" />
-          <strong className='fs-4'>{props.title}</strong>
+          <img src="https://img.icons8.com/color/512/txt.png" alt="text-logo" width="50" height="47" className="d-inline-block align-text-center" />
+          <strong className='fs-4 mx-3'>{props.title}</strong>
           </Link>
         {/* <a className="navbar-brand" href="#/">{props.title}</a> */}
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -19,11 +19,11 @@ export default function Navbar(props) {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <Link className="nav-link active" aria-current="page" to="/"><strong>Home</strong></Link>
+              <Link className="nav-link active mx-2" aria-current="page" to="/"><strong>Home</strong></Link>
               {/* <a className="nav-link active" aria-current="page" href="#/">Home</a> */}
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/about"><strong>{props.aboutText}</strong></Link>
+              <Link className="nav-link mx-2" to="/about"><strong>{props.aboutText}</strong></Link>
             </li>
           </ul>
           {/* <form className="d-flex" role="search">
@@ -32,8 +32,9 @@ export default function Navbar(props) {
           </form> */}
 
           <div className={`form-check form-switch text-${props.mode === 'light' ? 'dark':'light'}`}>
-            <input className="form-check-input" onClick = {props.toggleMode} type="checkbox" role="switch" id="flexSwitchCheckChecked"/>
-              <label className="form-check-label" htmlFor="flexSwitchCheckChecked">Enable Dark Mode</label>
+            <input className="form-check-input my-2" onClick = {props.toggleMode} type="checkbox" role="switch" id="flexSwitchCheckChecked" />
+              {/* <label className="form-check-label" htmlFor="flexSwitchCheckChecked">Enable Dark Mode</label> */}
+              <img src="https://cdn-icons-png.flaticon.com/512/7645/7645197.png" alt="darkModeIcon" width="30" height="30" className=" mx-2 align-text-bottom" />
           </div>
           
         </div>
